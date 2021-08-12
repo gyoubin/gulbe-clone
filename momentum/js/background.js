736d172d4c01c463b937290dd35680d9074c217a
gyoubin <87419428+gyoubin@users.noreply.github.com>
2021-08-12 15:32:44 +0900

    Delete momentum directory

--- momentum/js/background.js
+++
@@ -1,9 +0,0 @@
-const images = ["0.jpg","1.jpg","2.jpg"];
-
-const choseImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-
-bgImage.src = "img/" + choseImage;
-
-document.body.appendChild(bgImage);
